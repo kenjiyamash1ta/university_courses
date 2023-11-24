5c87cfa2ebd44d1d532bc9322514f1aea9de9942
yamash1ta <mrsight@yandex.ru>
2023-11-24 18:58:58 +0300

    6.0.1

--- 5th_homework/longest_nulls_chain/longest_nulls_chain.cpp
+++ 5th_homework/longest_nulls_chain/longest_nulls_chain.cpp
@@ -27,4 +27,19 @@ int main(){
         count++;
         vec.push_back(num);
     }
+    int cur = 0,maxc = 0;
+    for (int i = 1; i < vec.size(); i++) {
+        if(vec[i] == 0 && cur == 0){
+            cur++;
+        }
+        else if(vec[i] == vec[i - 1] && vec[i] == 0) {
+            cur++;
+        } 
+        else {
+            maxc = max(maxc, cur);
+            cur = 0;
+        }
+    }
+
+    cout << maxc;
 }
